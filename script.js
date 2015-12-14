4754875afb11f1b4cbdcc3d8ea37c70c23c714e4
Stephen Way <scway@thenavisway.com>
2015-12-14 15:12:15 -0800

    Epic compile

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 /* eslint-env browser */
-/* global Prism */
+/* global examples, Prism */
 
 document.addEventListener('DOMContentLoaded', function () {
-	Array.prototype.forEach.call(document.querySelectorAll('pre code'), function (code) {
-		var hasExample = code.className.match(/(?:^|\s)lang-(\w+)_example(?:\s|$)/) && true || false;
-		var language   = (code.className.match(/(?:^|\s)lang-(\w+?)(?:_example|\s|$)/) || [])[1];
+	Array.prototype.forEach.call(document.querySelectorAll('pre code[class^="lang"]'), function (code) {
+		// set pre, wrap, opts, and get meta data from code
+		var pre  = code.parentNode;
+		var wrap = pre.parentNode.insertBefore(document.createElement('figure'), pre);
+		var conf = {};
+		var text = String(code.textContent || code.innerText || '');
 
-		var html = code.innerText;
+		// get meta data from code class
+		code.className.replace(/^lang-(\w+)(?::(\w+))?/, function ($0, $1, $2) {
+			if ($2) return 'example:' + $2 + ',lang:' + $2;
 
-		if (language && language in Prism.languages) {
-			code.innerHTML = Prism.highlight(html, Prism.languages[language]);
-		}
+			if ($1 === 'example') return 'example:html';
 
-		if (hasExample) {
-			var example = document.createElement('div');
+			return 'lang:' + $1;
+		}).split(/\s*,\s*/).forEach(function (opt) {
+			opt = opt.split(':');
 
-			example.className = 'example';
+			conf[opt.shift().trim()] = opt.join(':').trim();
+		});
 
-			example.innerHTML = html;
+		code.removeAttribute('class');
 
-			code.parentNode.parentNode.insertBefore(example, code.parentNode);
+		wrap.appendChild(pre);
+
+		// conditionally syntax highlight code
+		if (conf.lang in Prism.languages) code.innerHTML = Prism.highlight(text, Prism.languages[conf.lang]);
+
+		// conditionally create code examples
+		if (conf.example in examples.lang) {
+			examples.lang[conf.example](pre, text, conf);
+
+			if (!(conf.lang in Prism.languages)) wrap.removeChild(pre);
 		}
 	});
 });
-
